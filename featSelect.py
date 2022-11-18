870f5e7513c4e7accdbd9ecdd7decc4d15241598
2000rick <qipeng.liang.edu@gmail.com>
2022-11-17 18:09:27 -0800

    z normalization

--- featSelect.py
+++ featSelect.py
@@ -1,3 +1,5 @@
+import scipy.stats as stats
+
 # Load data
 data = []
 dataFile = open('CS170_Small_Data__96.txt', 'r')
@@ -9,5 +11,10 @@ dataFile.close()
 for element in data:
     print(element)
 
+for element in data: # z normalization
+    element[1:] = stats.zscore(element[1:])
+
+for element in data:
+    print(element)
 print(f'Number of rows: {len(data)}')
 print(f'Number of features: {len(data[0])-1}')
