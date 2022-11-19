537223214718fc7ee662cf262855205f3a03cd6c
2000rick <qipeng.liang.edu@gmail.com>
2022-11-18 17:42:01 -0800

    honestly not sure what's wrong

--- featSelect.py
+++ featSelect.py
@@ -1,54 +1,51 @@
-import scipy.stats as stats
+import math
 
-def ReadFile():
-    # Load data
+def ReadFile(): # Load data
     data = []
-    dataFile = open('CS170_Small_Data__96.txt', 'r')
+    fileName = 'CS170_Small_Data__96.txt'
+    dataFile = open(fileName, 'r')
     for y in dataFile:
-        entry = [float(num) for num in y.split()] #one row of data
+        entry = [float(num) for num in y.split()] #one row/instance
         data.append(entry)
     dataFile.close()
-
-    for element in data: # z normalization
-        element[1:] = stats.zscore(element[1:])
     return data
 
-def NearestNeighbor(data, features, k=1):
-    # features: set of features to use
+def NearestNeighbor(data, features): #using leave-one-out cross validation
+    # features: the set of features to use/evaluate
     success = 0
     for i in range(len(data)):
-        # find nearest neighbor
         minDist = float('inf')
-        minIdx = -1
+        minLocation = 10e6
         for j in range(len(data)):
             if i != j:
-                distance = 0
+                distance = 0.0
                 for feature in features:
-                    distance += abs(data[i][feature] - data[j][feature])
+                    distance += (data[i][feature] - data[j][feature]) ** 2
+                distance = math.sqrt(distance)
                 if distance < minDist:
                     minDist = distance
-                    minIdx = j
+                    minLocation = j
         # check if success or failure
-        if data[i][0] == data[minIdx][0]:
+        if data[i][0] == data[minLocation][0]:
             success += 1
     accuracy = success / len(data)
     return accuracy
 
-def FowardSelection(data):
+def FowardSelection(data, defaultRate):
     numFeatures = len(data[0]) # number of features + 1
     currState = set() # current state of features
-    best = NearestNeighbor(data, currState)
+    best = defaultRate
     bestState = currState
-    print("Initial accuracy (default rate): ", best)
-    #iteratively add/select features starting from empty set (no features-default rate)
+    #iteratively add/select features
     for i in range(1, numFeatures):
         states = list()
         accuracies = list()
         for j in range(1, numFeatures):
-            if j not in currState:
+            if j not in currState: #if j isn't already added
                 tempState = currState.copy()
                 tempState.add(j)
                 states.append(tempState)
+        # print("states: ", states)
         for state in states:
             accuracy = NearestNeighbor(data, state)
             accuracies.append(accuracy)
@@ -56,24 +53,35 @@ def FowardSelection(data):
                 best = accuracy
                 bestState = state
         idx = accuracies.index(max(accuracies))
-        print("For i==", i, "Best feature set: ", states[idx], "Accuracy: ", accuracies[idx])
+        # print("Accuracy list:", accuracies)
+        # print("index:", idx)
+        print("For i ==", i, "Best feature set: ", states[idx], "Accuracy:", accuracies[idx])
         currState = states[idx]
     print("Best accuracy: ", best)
     print("Best state: ", bestState)
-            
 
+def testSelect(data, defaultRate):
+    numFeatures = len(data[0]) # number of features + 1
+    currState = set() # current state of features
+    best = defaultRate
+    bestState = currState
+    accuracy = NearestNeighbor(data, {1,5,4})
+    print("Test Accuracy: ", accuracy)
+    if accuracy > best:
+        best = accuracy
+        bestState = {1,5,4}
+    print("Best accuracy: ", best)
+    print("Best state: ", bestState)       
 
 def main():
     data = ReadFile()
-    ones = [i for i in data if i[0] == 1]
-    print("Number of 1s: ", len(ones))
-    twos = [i for i in data if i[0] == 2]
-    print("Number of 2s: ", len(twos))
-    print("Default rate should be: ", max(len(ones), len(twos)) / len(data))
-    FowardSelection(data)
+    print(f'This dataset has {len(data[0])-1} features, with {len(data)} instances.')
+    ones = [1 for i in data if i[0] == 1.0]
+    twos = [1 for i in data if i[0] == 2.0]
+    defaultRate = max(len(ones), len(twos)) / len(data)
+    print("Default rate: ", defaultRate)
+    # FowardSelection(data, defaultRate)
+    testSelect(data, defaultRate)
 
 if __name__ == "__main__":
     main()
-
-# print(f'Number of rows: {len(data)}')
-# print(f'Number of features: {len(data[0])-1}')
